bc0e824428d17c742539825f185f6cf121d42fd1
JoanCaballero <150661152+JoanCaballero@users.noreply.github.com>
2023-12-28 11:24:59 +0100

    Update Main.java

--- src/n1exercici3/Main.java
+++ src/n1exercici3/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        gestioExam gE = new gestioExam();
+        GestioExam gE = new GestioExam();
         gE.nomUsuari();
         gE.crearExam();
         gE.imprimirPuntuacio();
